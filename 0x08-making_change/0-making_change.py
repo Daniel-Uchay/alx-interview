f923b34bad38c312ecd606174825e097ac56f093
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-08-25 07:33:57 +0100

    optimized version

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-'''
+"""
+Task: Change comes from within
 Given a pile of coins of different values,
-determine the fewest number of coins needed to meet
-a given amount total.
-'''
-import sys
+determine the fewest number of coins needed to
+meet a given amount total
+"""
+
 
 def makeChange(coins, total):
-    '''
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
     Return: fewest number of coins needed to meet total
-    If total is 0 or less, return 0
-    If total cannot be met by any number of coins you have, return -1
-    '''
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
+    """
     if total <= 0:
         return 0
-    
-    coin_count = [sys.maxsize] * (total + 1)
-    coin_count[0] = 0
-    
-    num_coins = len(coins)
+
+    newVal = total + 1
+    store = {0: 0}
+
     for i in range(1, total + 1):
-        for j in range(num_coins):
-            if coins[j] <= i:
-                sub_coin_count = coin_count[i - coins[j]]
-                if sub_coin_count != sys.maxsize:
-                    coin_count[i] = min(coin_count[i], sub_coin_count + 1)
-    
-    if coin_count[total] == sys.maxsize:
+        store[i] = newVal
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            store[i] = min(store[current] + 1, store[i])
+
+    if store[total] == total + 1:
         return -1
-    
-    return coin_count[total]
+
+    return store[total]
